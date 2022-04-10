b9647200a506199ca83e261d67207d9320036b9c
Rodrigues <allan.da.s.rodrigues@accenture.com>
2022-04-10 10:47:55 -0300

    Init

---
+++ src/test/java/br/com/elo7/explorandomarte/ExplorandoMarteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo7.explorandomarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorandoMarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
